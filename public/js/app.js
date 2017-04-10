559aec742fd61cba6a4d1d50cdb5ad44dc1291a0
Puneet Thakur <puneetthakur@3189241.no-reply.drupal.org>
2017-04-10 17:44:01 +0530

    adding chat room and name

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,23 @@
+var name = getQueryVariable('name') || 'Anonymous';
+var room = getQueryVariable('room');
+
+console.log(name);
+console.log(room);
+
 var socket = io();
 socket.on('connect', function () {
 	console.log('connected to Socket.io server');
 });
 
 socket.on('message', function (message) {
+	var momentTimestamp = moment.utc(message.timestamp);
 	console.log("New Message Recieved: " + message.text);
-	$('div.message').append('<p>' + message.text + '</p>');
+
+	$message = $('div.message');
+
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format("h:mm a") + '</strong></p>');
+	$message.append('<p>'+ message.text +'</p>');
+	// $('div.message').append('<p><strong>' + momentTimestamp.local().format("h:mm a") + '</strong> ' + message.text + '</p>');
 });
 
 $form = $('#message-form');
@@ -16,6 +28,7 @@ $form.on('submit', function (event) {
 	var $message = $form.find('input[name="message"]');
 
 	socket.emit('message', {
+		name: name,
 		text: $message.val()
 	});
 
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -9,12 +10,15 @@ app.use(express.static(__dirname + '/public'));
 io.on('connection', function (socket){
 	socket.on('message', function (message) {
 		console.log("Message Recieved: " + message.text);
+		message.timestamp = moment().valueOf();
 		io.emit('message', message);
 	});	
 
-	// socket.emit('message', {
-	// 	text: "Welcome to the chat application"
-	// });
+	socket.emit('message', {
+		name:'System',
+		text: "Welcome to the chat application",
+		timestamp: moment().valueOf()
+	});
 });
 
 http.listen(PORT, function (){
