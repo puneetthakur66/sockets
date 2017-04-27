bc96e25115c7cc6c1372abfd9a973148e51d959d
Puneet Thakur <puneetthakur@3189241.no-reply.drupal.org>
2017-04-27 15:22:09 +0530

    chat

--- public/js/app.js
+++ public/js/app.js
@@ -1,36 +1,43 @@
 var name = getQueryVariable('name') || 'Anonymous';
 var room = getQueryVariable('room');
-
-console.log(name);
-console.log(room);
-
 var socket = io();
+
+console.log(name + ' wants to join ' + room);
+
+// Update h1 tag
+jQuery('.room-identifier').text(room);
+
 socket.on('connect', function () {
-	console.log('connected to Socket.io server');
+	console.log('Conncted to socket.io server!');
+	socket.emit('joinRoom', {
+		name: name,
+		room: room
+	});
 });
 
 socket.on('message', function (message) {
 	var momentTimestamp = moment.utc(message.timestamp);
-	console.log("New Message Recieved: " + message.text);
+	var $message = jQuery('.message');
 
-	$message = $('div.message');
+	console.log('New message:');
+	console.log(message.text);
 
-	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format("h:mm a") + '</strong></p>');
-	$message.append('<p>'+ message.text +'</p>');
-	// $('div.message').append('<p><strong>' + momentTimestamp.local().format("h:mm a") + '</strong> ' + message.text + '</p>');
+	$message.append('<p><strong>' + message.name + ' ' + momentTimestamp.local().format('h:mm a') + '</strong></p>');
+	$message.append('<p>' + message.text + '</p>');
 });
 
-$form = $('#message-form');
+// Handles submitting of new message
+var $form = jQuery('#message-form');
 
 $form.on('submit', function (event) {
 	event.preventDefault();
 
-	var $message = $form.find('input[name="message"]');
+	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
 		name: name,
 		text: $message.val()
 	});
 
-	$message.val("");
-}); 
+	$message.val('');
+});
